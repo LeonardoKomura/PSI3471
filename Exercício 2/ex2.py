a2d32700002b8db3289b921a6086c2c68859c566
Leonardo Isao Komura <leokomura@gmail.com>
2022-05-01 17:03:40 -0300

    ex2 finalizado

--- Exercício 2/ex2.py
+++ Exercício 2/ex2.py
@@ -1,53 +1,66 @@
+# --------------------------------------------------------------
+# André Lucas Pierote Rodrigues Vasconcelos - NUSP: 11356540
+# Leonardo Isao Komura                      - NUSP: 11261656
+# --------------------------------------------------------------
+
 import numpy as np
 import pandas as pd
+
+Nb = 20            # mini-batch size
+Ne = 2000          # número de épocas
+eta = 0.005
 
 data_df = pd.read_csv("circles_and_squares.csv")
 data_df.head()
 data = data_df.to_numpy()
 data_df.shape
-#print(data)
+print(data)
 
+coluna1 = np.ones((800,1))
 Xd = data[:800, :]
-dt = Xd[:, -1]
-d = np.reshape(dt, [-1,1]) 
-Xd = np.delete(Xd, -1, axis=1)
-#print(Xd)
+print(Xd)
 
 Xd_test = data[800:, :]
-yt_test = Xd_test[:, -1]
-y_test = np.reshape(yt_test, [-1,1])
+y_test = Xd_test[:, -1]
+#y_test = np.reshape(yt_test, [-1,1])
 Xd_test = np.delete(Xd_test, -1, axis=1)
-#print(Xd_test)
+coluna = np.ones((200,1))
+x_test = np.append(coluna, Xd_test, axis = 1)
+#x_test = np.transpose(xt_test)
+print(x_test)
 
-mb_size = 200           # mini-batch size
-ne = int(800/mb_size)   # número de épocas
-W = np.ones((401, 1))/5
-novoW = np.ones((401, 1))/5
-coluna1 = np.ones((mb_size,1))
-#print(coluna1)
+w = np.zeros(401)
+v = np.zeros(800)
+y = np.zeros(800)
+e = np.zeros(800)
 
-eta = 1
-
-for i in range(ne):
-    xt_incomplete = Xd[((i*mb_size)):(i+1)*mb_size, :] # 200x400
-    xt = np.append(coluna1, xt_incomplete, axis = 1) # 200x401
-    #print(xt)
-    x = np.transpose(xt) # 401x200
+for i in range(1, Ne):
+    np.random.permutation(Xd)[:]
+    #x = np.transpose(xt)
     #print("X: \n", x)
-    d_mb = d[i*mb_size, 0]
+    #print(x[j])
     
-    for j in range(mb_size):
-        W = novoW
-        x1 = x[:, j]
-        v = np.matmul(np.transpose(x1), W)
+    d = Xd[:, -1]
+    #d = np.reshape(dt, [-1,1]) 
+    
+    x = np.delete(Xd, -1, axis=1)
+    x = np.append(coluna1, x, axis = 1) # 100x401
+    
+    for j in range(int(800/Nb)-1):
+        v = np.matmul(x[j], w)
+        #print(j)
         #print(v)
-        y = (np.exp(v)-np.exp(-v))/(np.exp(v)+np.exp(-v))
+        y = np.sign(v)
         #print(y)
-        e = np.subtract(d_mb, y)
-        exp2v = np.exp(2*v)
-        exp2v_1 = (exp2v+1)**2
-        d_phi = exp2v/exp2v_1
-        #print(d_phi)
-        delta = np.dot(np.matrix.flatten(d_phi), np.matrix.flatten(e))
-        Delta = delta*xt
-        novoW = W + eta*Delta
+        e = d-y
+        #print(e)
+        w = np.add(w, (eta/Nb)*np.matmul(np.transpose(x), e))
+
+result = np.zeros(200)
+n_erros = 0
+for i in range(200):
+    result[i] = np.sign(np.matmul(x_test[i], w))
+    erro = y_test[i] - result[i]
+    if erro != 0: 
+        n_erros = n_erros + 1
+print(n_erros)
--- teste.py
+++ teste.py
@@ -1,14 +1,20 @@
 import math
 import numpy as np
-A = np.array([1, 2, 3, 4, 5])
-A1 = np.reshape(A, [-1,1]) 
-At = np.transpose(A)
-print(A)
-print(A1)
-B = np.array([1, 1, 1, 1, 1])
-B1 = np.reshape(B, [-1,1]) 
-print(B1)
-C = np.add(A1,B1)
-print(C)
-D = A1+1
-print(D)
+#A = np.array([1, 2, 3, 4, 5])
+#A1 = np.reshape(A, [-1,1]) 
+#At = np.transpose(A)
+#print(A)
+#print(A1)
+#B = np.array([1, 1, 1, 1, 1])
+#B1 = np.reshape(B, [-1,1]) 
+#print(B1)
+#C = np.add(A1,B1)
+#print(C)
+#D = A1+1
+#print(D)
+
+a = np.array([[1,2,3],[2,2,2],[5,5,5]])
+print(a)
+for i in range(3):
+    b = a[i:(i+1), 0:3]
+    print(b)
