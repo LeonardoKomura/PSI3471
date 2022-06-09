fc6bf35bae54450097592d789dd2a780d61f137b
Leonardo Isao Komura <leokomura@gmail.com>
2022-06-08 21:25:11 -0300

    p1_entrada

--- teste.py
+++ teste.py
@@ -1,14 +1,4 @@
 import math
 import numpy as np
-A = np.ones((3, 1))
-print(A)
-
-B = A + [[1], [2], [3]]
-print(B)
-
-AF = A.flatten(order='C')
-BF = B.flatten(order='C')
-print(AF)
-print(BF)
-C = np.dot(AF, BF)
-print(C)
+tensor = np.ndarray(shape=(1,1), dtype=float)
+print(tensor)
